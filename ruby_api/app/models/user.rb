420b8cb1c94df20cf89678066113c085a73fca36
Roy Nilsson <rn222cx@student.lnu.se>
2016-01-27 21:43:00 +0000

    change folder name

---
+++ ruby_api/app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ActiveRecord::Base
+  has_many :stories, dependent: :destroy # remove users stories when delete, same as cascade
+  
+  has_secure_password
+  validates :password_digest, length: { minimum: 3 }
+               
+  before_save { self.email = email.downcase }       
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, 
+             presence: true,
+             length: { maximum: 50 },
+             format: { with: VALID_EMAIL_REGEX },
+             uniqueness: { case_sensitive: false }
+              
+  
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end        
+end
---
+++ ruby_api/db/migrate/20160113121606_create_stories.rb
@@ -0,0 +1,16 @@
+class CreateStories < ActiveRecord::Migration
+  def change
+    create_table :stories do |t|
+      #t.references :user
+      t.references :user, index: true, foreign_key: true
+      
+      t.string "title", :limit => 50
+      t.text "description"
+      t.float "longitude"
+      t.float "latitude"
+      t.timestamps null: false
+    end
+    
+    add_index :stories, [:user_id, :created_at]
+  end
+end
